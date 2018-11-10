a03d901fabfe5737b95c750701e7759f47f37832
aei2000 <aydin.iverson@gmail.com>
2018-11-09 19:19:40 -0600

    Created Laura
    Laura.java created

---
+++ src/me/commonsenze/Platformer/Objects/Characters/Laura.java
@@ -0,0 +1,52 @@
+package me.commonsenze.Platformer.Objects.Characters;
+
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.Rectangle;
+import java.util.ArrayList;
+
+import me.commonsenze.Platformer.Main;
+import me.commonsenze.Platformer.Objects.Block;
+import me.commonsenze.Platformer.Util.GameObject;
+import me.commonsenze.Platformer.Util.Renderable;
+import me.commonsenze.Platformer.Util.Enums.Role;
+
+
+public class Laura extends GameObject implements Renderable{
+
+	public Laura(Role role, Rectangle character) {
+		super(role, character);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public void tick() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void render(Graphics g) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void jump() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void gravity(ArrayList<Block> blocks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void walk(ArrayList<Block> blocks) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
