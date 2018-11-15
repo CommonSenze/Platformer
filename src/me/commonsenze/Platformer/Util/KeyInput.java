dbf932c80e0912e267609150c704777091c0c52f
aei2000 <aydin.iverson@gmail.com>
2018-11-15 09:00:21 -0600

    agay
    gay

--- src/me/commonsenze/Platformer/Util/KeyInput.java
+++ src/me/commonsenze/Platformer/Util/KeyInput.java
@@ -8,10 +8,11 @@ import me.commonsenze.Platformer.Main;
 import me.commonsenze.Platformer.Util.Enums.Role;
 
 public class KeyInput extends KeyAdapter {
-	//setting a global double jump variable (maybe we can use it for challenges later, but also so it's only defined once.
-	int doubleJump;
+	// setting a global double jump variable (maybe we can use it for challenges
+	// later, but also so it's only defined once.
+	private int doubleJump;
 	private Handler handler;
-	
+
 	public KeyInput(Handler handler) {
 		this.handler = handler;
 	}
@@ -27,23 +28,19 @@ public class KeyInput extends KeyAdapter {
 						object.jump();
 						object.setJumping(true);
 						doubleJump = 1;
-					}else if (object.getRole()== Role.SARAH)
-						{
-						if (!(doubleJump==0))
-						{
-					
-						
-						object.jump();
-						doubleJump = 0;
-					}
+					} else if (object.getRole() == Role.SARAH) {
+						if (!(doubleJump == 0)) {
+							object.jump();
+							doubleJump = 0;
 						}
-					
+					}
+
 				}
-				
+
 				if (key == KeyEvent.VK_Q) {
 					GameData.setCharacter(handler.getObject(object.getIntX(), "Left").getRole());
 				}
-				
+
 				if (key == KeyEvent.VK_E) {
 					GameData.setCharacter(handler.getObject(object.getIntX(), "Right").getRole());
 				}
@@ -55,16 +52,16 @@ public class KeyInput extends KeyAdapter {
 					object.setVelocity(5);
 				}
 				if (key == KeyEvent.VK_F) {
-					Main.CAMERA.setZoom((Main.CAMERA.getZoom()+0.1));
+					Main.CAMERA.setZoom((Main.CAMERA.getZoom() + 0.1));
 				}
 				if (key == KeyEvent.VK_R) {
-					Main.CAMERA.setZoom((Main.CAMERA.getZoom()-0.1));
+					Main.CAMERA.setZoom((Main.CAMERA.getZoom() - 0.1));
 				}
 				break;
 			}
 		}
 	}
-	
+
 	@Override
 	public void keyReleased(KeyEvent e) {
 		int key = e.getKeyCode();
@@ -74,16 +71,19 @@ public class KeyInput extends KeyAdapter {
 			if (object.getRole() == GameData.getSelectedCharacter()) {
 
 				if (key == KeyEvent.VK_A) {
-					if (object.getVelocity() == -5) object.setVelocity(0);
+					if (object.getVelocity() == -5)
+						object.setVelocity(0);
 				}
 				if (key == KeyEvent.VK_D) {
-					if (object.getVelocity() == 5) object.setVelocity(0);
+					if (object.getVelocity() == 5)
+						object.setVelocity(0);
 				}
-				
+
 				if (Main.CAMERA.getSpeed() != 0) {
 					Main.CAMERA.setSpeed(0);
 				}
-			} else object.setVelocity(0);
+			} else
+				object.setVelocity(0);
 		}
 	}
 }
